076e76153068c973e5e187a29c64820e13915c29
v4ljok <v4ljok.c@gmail.com>
2024-09-29 13:43:08 +0300

    loading environment variable from .env file

---
+++ backend/env.php
@@ -0,0 +1,27 @@
+<?php
+
+function loadEnv($filePath) {
+    if (!file_exists($filePath)) {
+        return;
+    }
+
+    $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) {
+            continue;
+        }
+
+        list($name, $value) = explode('=', $line, 2);
+        $name = trim($name);
+        $value = trim($value);
+
+        if (!array_key_exists($name, $_ENV)) {
+            $_ENV[$name] = $value;
+        }
+    }
+}
+
+
+loadEnv(__DIR__ . '/.env');
+
+?>
